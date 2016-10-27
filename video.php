8114da17454e70824dfaac13a9a24d1ba778ee84
MFabien <fabienmehlinger.pro@gmail.com>
2016-10-27 10:05:16 +0200

    afficher les videos

---
+++ video.php
@@ -0,0 +1,15 @@
+<?php include'config.php';
+
+        $req = $dbconnexion->query("SELECT * FROM video");
+
+
+
+  while ($row = $req->fetch()){
+      ?>
+<iframe width="360" height="215" src="<?php echo $row['lien'];?>" frameborder="0" allowfullscreen></iframe><br>
+
+   <?php echo $row['Titre'];?><br>
+
+
+ <?php }
+?>
