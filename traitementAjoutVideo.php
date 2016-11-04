870a27eaa0d64572ae99b9ef5072be7368fa125b
MFabien <fabienmehlinger.pro@gmail.com>
2016-11-04 09:01:39 +0100

    ajout de la page traitement des video

--- traitementAjoutVideo.php
+++ traitementAjoutVideo.php
@@ -1,7 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mehlingerf
- * Date: 02/11/16
- * Time: 10:37
- */
+include('config.php');
+
+if (!empty($_POST) && isset($_POST['titre']) && isset($_POST['lien']) && isset($_POST['mot_de_passe'])) {
+    $titre = htmlspecialchars($_POST['titre']);
+    $lien = htmlspecialchars($_POST['lien']);
+    $mdp = htmlspecialchars($_POST['mdp']);
+
+    foreach ($_POST['categorie'] as $valeur) {
+
+        $requete ='INSERT INTO video( titre, lien, mot_de_passe, categorie_video ) VALUES (' . $titre . ', ' . $lien . ', ' . $mdp . ', ' . $valeur . ')';
+
+
+
+    }
+
+    //if(!$_POST['categorie']){
+      //  echo 'Aucune checkbox n\'a été cochée';
+    //}
+
+ if($dbconnexion->query($requete)) {
+    // header('Location: index.php?Videoajoutee');
+    echo'oui';
+
+}
+else {
+    //  header('Location: index.php?Videoerror');
+    echo 'error';
+    }
+}
+
+
+
+
+
+
+
+
+
+?>
+
