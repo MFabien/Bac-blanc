e41916592ca38e73e147484dcbe930d4c520df88
MFabien <fabienmehlinger.pro@gmail.com>
2016-11-04 08:58:18 +0100

    midification pour ajouter dans des categories

--- ajout_video.php
+++ ajout_video.php
@@ -1,39 +1,26 @@
-<?php include'index.php';?>
-
-    <form action='' method='post'>
+<form action='traitementAjoutVideo.php' method='POST'>
         <label>Titre:</label><br>
         <input type="text" name="titre"><br>
         <label>Lien:</label><br>
         <input type="text" name="lien"><br>
         <label>mot_de_passe(falcultatif):</label><br>
         <input type="password" name="mdp"><br>
+        <?php
+        $sql= "SELECT * FROM categorie";
+        $req = $dbconnexion->query($sql);
+
+
+        // on envoie la requête
+        while ($row = $req->fetch()) {
+        ?>
+        <label class="btn btn-danger">
+         <input type="radio" name="categorie " value="<?= $row['id_categorie'];?>"> <?= $row['nom_categorie']; ?>
+        </label>
+            <?php
+        }
+        ?>
+        <br>
+        <br>
+        <br>
         <input type="submit" name="addvideo" value="Ajouter">
     </form>
-
-
-
-    <?php include 'config.php';
-
-    if (isset($_POST['addvideo'])){
-        $titre =  $_POST['titre'];
-        $lien = $_POST['lien'];
-        $mdp = $_POST['mdp'];
-
-
-        $sql = "INSERT INTO video (titre,lien,mot_de_passe)VALUES('".$titre."','".$lien."','".$mdp."')";
-        if($dbconnexion->query($sql)){
-            echo "Video envoyer";
-
-        }else{
-            echo "echec";
-        }
-
-
-
-
-
-    }
-
-
-    ?>
-
